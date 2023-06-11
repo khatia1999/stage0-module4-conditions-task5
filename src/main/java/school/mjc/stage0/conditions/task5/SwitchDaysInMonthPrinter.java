367ee4c29748b19cbb43f8b3b2fca9ba4349b540
khatia ormotsadze <khatiaormotsadze99@gmail.com>
2023-06-12 00:18:20 +0400

    git commit -m "Modify BasicSwitch.java" -m "Modify BasicSwitchBreaks" -m "Modify LeapYearPrinter.java" -m "Modify RangeNumbersPrinter.java" -m "Modify SwitchDaysInMonthPrinter.java"

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,34 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+
+    switch (month){
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+
+            System.out.println("31 days");
+            break;
+
+        case 2:
+            System.out.println("28 days");
+            break;
+
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            System.out.println("30 days");
+            break;
+
+        default:
+            System.out.println("Please enter a number between 1 - 12");
+            break;
+    }
+
     }
 }
